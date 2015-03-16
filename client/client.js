7a9578206daa7b2ab30e35da062a032a9b8669e8
nikwhite <underscore.nik@gmail.com>
2015-03-16 17:51:00 -0500

    formatting

--- client/client.js
+++ client/client.js
@@ -2,37 +2,39 @@
  * [description]
  * @return {[type]} [description]
  */
-(function(){
-	// not currently in use
-	var api = window.netflixController = {};
-	var socket = api.socket = io();
+;(function(){
+	'use strict'
 
-	var lastTime = (new Date()).getTime();
+	var api = window.netflixController = {}
+	var socket = api.socket = io()
+
+	var lastTime = (new Date()).getTime()
 
 	function reconnect() {
-		socket = io({ forceNew: true });
+		socket = io({ forceNew: true })
 	}
 
-	var interval = setInterval(function() {
-		var currentTime = (new Date()).getTime();
-		if ( currentTime > (lastTime + 60000) ) {  // wait 1 minute
-			alert('reconnecting');
-			reconnect();
+	setInterval(function() {
+		var currentTime = (new Date()).getTime()
+		// if the timeout is longer than 3000ms, the device has 
+		// fallen asleep and woken up.
+		if ( currentTime - lastTime >= 3000 ) {  
+			reconnect()
 		}
-	  lastTime = currentTime;
-	}, 1000);
+		lastTime = currentTime
+  	}, 1000)
 
 	// button events
 	$('body').on('click', '[data-event]', function (e){
-		e.preventDefault();
-		socket.emit( e.target.getAttribute('data-event'), e.target.getAttribute('href') );
-	});
+		e.preventDefault()
+		socket.emit( e.target.getAttribute('data-event'), e.target.getAttribute('href') )
+	})
 
 	// search event
 	$('#search').on('submit', function (e) {
-		e.preventDefault();
-		socket.emit('search', e.target.searchField.value);
-		e.target.searchField.blur();
-	});
+		e.preventDefault()
+		socket.emit('search', e.target.searchField.value)
+		e.target.searchField.blur()
+	})
 
-}());
+}())
